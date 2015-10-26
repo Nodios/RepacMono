7a885c5a95b7208237d4d391beadf6204b8874b9
Nodios <drepac@etfos.hr>
2015-10-26 19:02:27 +0100

    added DI module

---
+++ Lost.Service/DIModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Lost.Service.Common;
+
+namespace Lost.Service
+{
+    public class DIModule : Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILostService>().To<LostService>();
+        }
+    }
+}
